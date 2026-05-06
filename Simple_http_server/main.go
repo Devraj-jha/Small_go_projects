31045e1e21391900ebf2de9d09949b6406dffd1a
Devraj-jha <devrajjha100806@gmail.com>
2026-05-06 11:06:20 +0530

    server #1

--- Simple_http_server/main.go
+++ Simple_http_server/main.go
@@ -8,10 +8,13 @@ import (
 func main(){
 	
 	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello_you visted this server %s", r.URL.Path)
+	fmt.Fprintf(w, "hello_you visited this server %s\n", r.URL.Path)
+    fmt.Fprintf(w, "this is a backend server\n")
+   fmt.Fprintf(w, "this is made in Go 🌟\n")
+
 
 	})
 	fmt.Println("Server is running at http://localhost:8080")
-    http.ListenAndServe(":999", nil)
+    http.ListenAndServe(":8080", nil)
 
 }
