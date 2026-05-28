01845bb4d6a589d1e7814bc7c9ea3fbc083358c3
devraj-jha <devrajjha100806@gmail.com>
2026-05-28 16:01:08 +0530

    variables

---
+++ Go_sytax/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	//!Variables 
+	
+	// two ways to declare a variable 
+	var age int = 14  // var   name type = value
+	name := "djjha" // shorter name := value
+
+	age = 10 
+	fmt.Println(age)
+	fmt.Println(name)
+
+	// sum 
+
+	a:= 10
+	b := 20
+	
+	//take input => store in variable -> modify varibles -> produce output
+
+	//!
+	fmt.Println(a + b)
+	//age → [ 20 ]
+
+}
