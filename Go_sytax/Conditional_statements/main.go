2b4d446c444ea3f09d6dc087f64b877a47097252
devraj-jha <devrajjha100806@gmail.com>
2026-05-28 16:31:57 +0530

    conditional statements

---
+++ Go_sytax/Conditional_statements/main.go
@@ -0,0 +1,33 @@
+package main
+
+//store data -> make decision -> repeat actions
+
+
+import "fmt"
+
+func main(){
+	//? -> 1st example
+	age := 20
+
+	if age >= 18{
+		fmt.Println("Adult")
+
+	}else {
+		fmt.Println("kid")
+	}
+	println(age == 20)
+
+	//? -> 2nd example 
+
+	marks := 75
+
+	if marks >= 90 {
+		fmt.Println("topper")
+	}else if marks >= 70 {
+		fmt.Println("average")
+	}else {
+		fmt.Println("billioanire")
+
+	}
+
+}
